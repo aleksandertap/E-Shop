a33ee7d77ca63e483968ae05506b582cd4ff9262
Aleksander Tapsi <aleksander.tapsi@voco.ee>
2024-11-06 06:53:37 -0500

    product osa tehtud

---
+++ yl1/main.js
@@ -0,0 +1,17 @@
+class Product{
+    constructor(title, price, category){
+        this.title = title,
+        this.price = price,
+        this.category = category
+    }
+    describe(){
+        return `This is a ${this.title}. It costs $${this.price}. It is a ${this.category}`
+    }
+    static discountedPrice(price, discount){
+        return price - (price * discount / 100)
+    }
+}
+
+let laptop = new Product("sülearvuti", 800.50, "elektronika")
+console.log(laptop.describe())
+console.log(Product.discountedPrice(laptop.price, 10))
