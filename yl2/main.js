fd70a2b594ef80be7ba6a0b0b9d9114c262acb7c
aleksandertap <aleksander.tapsi@voco.ee>
2024-11-26 12:04:12 +0200

    kuvab tootenimesid

--- yl2/main.js
+++ yl2/main.js
@@ -9,17 +9,18 @@ import { Customer } from './Customer.js';
 
 // Loo mõned tooted
 
-const laptop = new Product(1, 'Sülearvuti', 1000, 'Elektroonika');
-
-const phone = new Product(2, 'Telefon', 500, 'Elektroonika');
-
+const products =[
+new Product(1, 'Sülearvuti', 999.99, 'Elektroonika'),
+new Product(2, 'Telefon', 499.99, 'Elektroonika'),
+new Product(3, 'Tahvelarvuti', 299.99, 'Elektroonika')
+]
 // Loo ostukorv ja lisa tooted
 
 const cart = new Cart();
 
-cart.addProduct(laptop, 1);
+// cart.addProduct(laptop, 1);
 
-cart.addProduct(phone, 2);
+// cart.addProduct(phone, 2);
 
 // Kuvage ostukorvi summa ja toodete arv
 
@@ -37,3 +38,27 @@ customer.placeOrder(cart);
 
 customer.printOrderHistory();
 
+
+document.title = 'My WebShop';
+const heading1 = document.getElementById("heading1")
+
+heading1.style.backgroundColor = "aqua";
+heading1.style.textAlign = "center";
+heading1.style.color = "orange"
+console.log(heading1)
+
+const mainDiv = document.getElementById("mainDiv")
+console.log(mainDiv)
+
+// Toodete kuvamine
+function displayProducts(){
+
+    products.forEach((product) =>{
+        const productCard = document.createElement("div");
+        productCard.innerHTML = `<h3>${product.name}</h3>`;
+        mainDiv.append(productCard);
+    }
+    )
+}
+
+displayProducts()
