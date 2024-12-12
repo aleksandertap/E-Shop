efa32848c6912e5d459e6f211123e44728ee1bf3
aleksandertap <aleksander.tapsi@voco.ee>
2024-12-12 08:21:22 +0200

    push favebutton

--- yl3/views/allProductsView.js
+++ yl3/views/allProductsView.js
@@ -30,16 +30,13 @@ export function displayProducts(products) {
     faveButton.textContent = "Lisa lemmikutesse";
     faveButton.onclick = (e) => {
       e.stopPropagation();
-      if (faveButton.textContent === "Lisa lemmikutesse"){
-        faveButton.textContent = "Eemalda lemmikutest"
+      if (faveButton.textContent === "Lisa lemmikutesse") {
+        faveButton.textContent = "Eemalda lemmikutest";
         costumerConstructor.toggleFavorites(product);
-    }
-        else if (faveButton.textContent === "Eemalda lemmikutest"){
-            faveButton.textContent = "Lisa lemmikutesse"
-            costumerConstructor.toggleFavorites(product);
-        }
-      
-      
+      } else if (faveButton.textContent === "Eemalda lemmikutest") {
+        faveButton.textContent = "Lisa lemmikutesse";
+        costumerConstructor.toggleFavorites(product);
+      }
     };
 
     productCard.append(faveButton);
