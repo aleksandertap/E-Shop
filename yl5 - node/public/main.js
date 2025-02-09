bc002717bb408d2bcddcae0432b87c2922b3605b
aleksandertap <aleksander.tapsi@voco.ee>
2025-02-09 14:32:27 +0200

    npm run dev working

--- yl5 - node/public/main.js
+++ yl5 - node/public/main.js
@@ -1,22 +1,10 @@
 // main.js
 
-import { Cart } from "./constructors/Cart.js";
 import { costumerConstructor, Customer } from "./constructors/Customer.js";
-import { favoritesView } from "./views/favoritesView.js";
-import { displayProductDetailView } from "./views/productDetailView.js";
-import { displayCartView } from "./views/cartView.js";
-
-import { Product } from "./constructors/Product.js";
 import { navigate } from "./router.js";
 import { displayProducts } from "./views/allProductsView.js";
 import { getAllCategory } from "./api.js";
-// Loo mõned tooted
 
-// const products = [
-//   new Product(1, "Sülearvuti", 999.99, "Elektroonika"),
-//   new Product(2, "Telefon", 499.99, "Elektroonika"),
-//   new Product(3, "Tahvelarvuti", 299.99, "Elektroonika"),
-// ];
 
 document.title = "My WebShop";
 
